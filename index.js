c4f53df9d9f7442efe9e0480efb9916972bf12d1
Vartika Jain <76568196+vartikajn07@users.noreply.github.com>
2022-03-26 18:33:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+https://l1pp6.csb.app/
